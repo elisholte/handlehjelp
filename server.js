c885336a3c3c5fe9b022c507bb3abc58fc3b2d70
elisholte <eliseholte2123@gmail.com>
2026-05-10 21:15:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const KASSAL_KEY = process.env.KASSAL_KEY || '';
 
 app.get('/api/search', async (req, res) => {
